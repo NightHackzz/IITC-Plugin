73c63c2c782d4a872ff41fb9a83d9511e3a79f06
卜部和敏 <urabe@mcomp.co.jp>
2017-12-06 15:41:24 +0900

    修正

--- basemap-gis-cyber-japan.user.js
+++ basemap-gis-cyber-japan.user.js
@@ -23,12 +23,12 @@ function wrapper(plugin_info) {
     if(typeof window.plugin !== 'function') window.plugin = function() {};
 
     plugin_info.buildName = 'gis-cyber-japan';
-    plugin_info.dateTimeVersion = '20171206.0001';
+    plugin_info.dateTimeVersion = '20171206.0002';
     plugin_info.pluginId = 'gis-cyber-japan';
 
 	window.plugin.mapGISCyberJapan = function () {};
 
-	window.plugin.mapGISCyberJapan.setup = function ()
+	var setup = function ()
 	{
 		// 国土地理院 tiles
 
@@ -46,14 +46,14 @@ function wrapper(plugin_info) {
 		  var layer = new L.TileLayer(url, gisOpt);
 		  layerChooser.addBaseLayer(layer, layers[url]);
 		}
-	}
+	};
+
 
     setup.info = plugin_info; //add the script info data to the function as a property
     if(!window.bootPlugins) window.bootPlugins = [];
     window.bootPlugins.push(setup);
     // if IITC has already booted, immediately run the 'setup' function
     if(window.iitcLoaded && typeof setup === 'function') setup();
-
 } // wrapper end
 // inject code into site context
 var script = document.createElement('script');
@@ -61,6 +61,7 @@ var info = {};
 if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
 script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
 (document.body || document.head || document.documentElement).appendChild(script);
+
 
 
 // PLUGIN END //////////////////////////////////////////////////////////
