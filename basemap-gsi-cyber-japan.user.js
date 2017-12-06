035802c5be72ee1d95a5df0090e4bc9419f590c9
卜部和敏 <urabe@mcomp.co.jp>
2017-12-06 17:22:10 +0900

    version8-1

--- basemap-gsi-cyber-japan.user.js
+++ basemap-gsi-cyber-japan.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-basemap-gsicyberjapan
 // @name           IITC plugin: GSI Cyber Japan map tiles
 // @category       Map Tiles
-// @version        0.0.8.20171206171600
+// @version        0.0.8.20171206172100
 // @namespace      https://github.com/NightHackzz/IITC-Plugin
 // @updateURL      https://raw.githubusercontent.com/NightHackzz/IITC-Plugin/master/basemap-gsi-cyber-japan.user.js
 // @downloadURL    https://raw.githubusercontent.com/NightHackzz/IITC-Plugin/master/basemap-gsi-cyber-japan.user.js
@@ -23,7 +23,7 @@ function wrapper(plugin_info) {
     if(typeof window.plugin !== 'function') window.plugin = function() {};
 
     plugin_info.buildName = 'gsi-cyber-japan';
-    plugin_info.dateTimeVersion = '20171206171600';
+    plugin_info.dateTimeVersion = '20171206172100';
     plugin_info.pluginId = 'gsi-cyber-japan';
 
 	var setup = function ()
